d281d90286e14ab34902b5e69ec73d13780b47d6
RAFA <rafa71802937@gmail.com>
2024-08-08 17:56:44 +0900

    [refactor] 함수 및 import 추가
    - SnapKit, Then 추가
    - setDelegate, registerCell, setCollectionView, addTarget, bind 함수 추가

--- BookTalk/BookTalk/Sources/Util/Base/BaseViewController.swift
+++ BookTalk/BookTalk/Sources/Util/Base/BaseViewController.swift
@@ -7,22 +7,45 @@
 
 import UIKit
 
-class BaseViewController: UIViewController {
+import SnapKit
+import Then
 
+class BaseViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         setNavigationBar()
         setViews()
         setConstraints()
+        setDelegate()
+        registerCell()
+        setCollectionView()
+        addTarget()
+        bind()
     }
-
+    
     /// navigation bar 설정
     func setNavigationBar() { }
-
+    
     /// view attributes 설정
     func setViews() { }
-
+    
     /// view hierarchy, constraints 설정
     func setConstraints() { }
+    
+    /// delegate 설정
+    func setDelegate() { }
+    
+    /// cell 등록
+    func registerCell() { }
+    
+    /// collection view 설정
+    func setCollectionView() { }
+    
+    /// button target 추가
+    func addTarget() { }
+    
+    /// data binding
+    func bind() { }
 }
