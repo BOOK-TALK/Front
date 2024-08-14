8b29b0fe0df7289f687848cc85e001b2f4ca8bf6
RAFA <rafa71802937@gmail.com>
2024-08-14 16:41:04 +0900

    [feat] 커스텀 레이블 구현

--- BookTalk/BookTalk/Sources/Util/PaddedLabel.swift
+++ BookTalk/BookTalk/Sources/Util/PaddedLabel.swift
@@ -5,4 +5,23 @@
 //  Created by RAFA on 8/14/24.
 //
 
-import Foundation
+import UIKit
+
+final class PaddedLabel: UILabel {
+    
+    var textInsets = UIEdgeInsets.zero {
+        didSet { invalidateIntrinsicContentSize() }
+    }
+    
+    override func drawText(in rect: CGRect) {
+        super.drawText(in: rect.inset(by: textInsets))
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        let size = super.intrinsicContentSize
+        return CGSize(
+            width: size.width + textInsets.left + textInsets.right,
+            height: size.height + textInsets.top + textInsets.bottom
+        )
+    }
+}
