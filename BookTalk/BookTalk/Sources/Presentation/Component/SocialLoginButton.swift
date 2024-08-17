1423080e5a7d8b3168d258cd682394fc658f7742
RAFA <rafa71802937@gmail.com>
2024-08-17 21:26:33 +0900

    [add] 로그인 타입 추가

---
+++ BookTalk/BookTalk/Sources/Presentation/Component/SocialLoginButton.swift
@@ -0,0 +1,8 @@
+//
+//  SocialLoginButton.swift
+//  BookTalk
+//
+//  Created by RAFA on 8/17/24.
+//
+
+import Foundation
---
+++ BookTalk/BookTalk/Sources/Presentation/Login/Enum/LoginType.swift
@@ -0,0 +1,12 @@
+//
+//  LoginType.swift
+//  BookTalk
+//
+//  Created by RAFA on 8/17/24.
+//
+
+import Foundation
+
+enum LoginType {
+    case apple, kakao
+}
